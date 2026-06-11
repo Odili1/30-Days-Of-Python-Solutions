393dbab49596d4f7be71a0b1cee7dac1b1f4375b
Odili1 <ezedigwegerald1@gmail.com>
2026-06-11 15:59:15 +0100

    update Day 26

--- Day_26.py
+++ Day_26.py
@@ -1,5 +1,20 @@
 # ----- PYTHON WEB -----
 
+# Deploying a Python Flask Project on Render
+
+# source venv/Scripts/activate
+# pip freeze > requirements.txt
+# pip install gunicorn
+# Run gunicorn app:app (gunicorn <filename>:<flask_instance>)
+
+# Create Render account
+# name => my-flask-app
+# Runtime => Python 3
+# Build COummand => pip install -r requirements.txt
+# Start Command => gunicorn app:app
+# Click Deploy Service => Generates the url for the project
+
+
 # Check out my "Python-for-Web" Repo for this implementation
 
 # ----- DONE -----
